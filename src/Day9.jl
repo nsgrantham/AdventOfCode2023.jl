b66c86effd526534962a14b69191d927bd36ad9d
Neal Grantham <neal@nsgrantham.com>
2023-12-09 07:53:19 -0800

    Simplify Day 9

--- src/Day9.jl
+++ src/Day9.jl
@@ -4,19 +4,14 @@ using AdventOfCode2023
 
 
 function extrapolate(x::Vector{Int})
-    y = Int[]
-    while true
-        push!(y, x[end])
-        x = diff(x)
-        all(==(0), x) && break
-    end
-    pop!(cumsum(reverse(y)))
+    all(==(0), x) && return 0
+    x[end] + extrapolate(diff(x))
 end
 
 function solve(input=pkgdir(AdventOfCode2023, "data", "Day9.txt"))
     xs = [parse.(Int, split(line)) for line in eachline(input)]
-    p1 = sum(extrapolate.(xs))
-    p2 = sum(extrapolate.(reverse.(xs)))
+    p1 = sum(extrapolate, xs)
+    p2 = sum(extrapolate, reverse.(xs))
     p1, p2
 end
 
